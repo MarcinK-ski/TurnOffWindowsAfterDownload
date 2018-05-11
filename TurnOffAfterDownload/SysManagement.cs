3a393fa42e103e1a01684ebfc74efe2f21d349df
Marcin <kalik95@gmail.com>
2018-05-11 23:55:18 +0200

    Small fixes

--- TurnOffAfterDownload/SysManagement.cs
+++ TurnOffAfterDownload/SysManagement.cs
@@ -7,40 +7,51 @@ namespace Kalikowo
 {
     static class SysManagement
     {
-        public static void countZeros(int zeroLimit = 10)
+        const string ALL_DISKS_DIRECTIVE = "_Total";
+
+        /*
+         * Shutdown PC after X seconds disk idle  ;   X = countSeconds
+         */
+        public static void countZeros(int countSeconds = 10)
         {
             string diskName = SelectDisk();
 
             PerformanceCounter performanceCounter = new PerformanceCounter("PhysicalDisk", "Disk Write Bytes/sec", diskName);
 
-            int countZeros = 0;
+            int countZeros = 0; //Zeros = idle state
 
-            Console.WriteLine("Let's begin... Reboot after: " + zeroLimit + " seconds");
+            Console.WriteLine("Let's begin... Reboot after: " + countSeconds + " seconds idle");
 
             while (true)
             {
-                float nextVal = performanceCounter.NextValue();
+                float nextVal = performanceCounter.NextValue(); //Get current activity value
                 Console.WriteLine(nextVal);
-                if (nextVal == 0)
+                if (nextVal == 0)   //Check is disk idle. 
                     countZeros++;
-                else
+                else                //If activity is detected
                     countZeros = 0;
 
                 Thread.Sleep(1000);
 
-                if (countZeros >= zeroLimit)
+                if (countZeros >= countSeconds) //When max seconds limit is reached, poweroff PC
                 {
-                    RebootingNow();
+                    ShutDownNow();
                     break;
                 }
             }
         }
 
-        public static void RebootingNow()
+        /*
+         * Shutdown OS
+         */
+        public static void ShutDownNow()
         {
             Process.Start("ShutDown", "/s");
         }
 
+        /*
+         * Display list of categories in PerformanceCounter
+         */
         public static void ListCategories()
         {
             foreach (var category in PerformanceCounterCategory.GetCategories())
@@ -57,11 +68,17 @@ namespace Kalikowo
             Console.ReadKey();
         }
 
+        /*
+         * Get disks names (ex. C:/, D:/, etc)
+         */
         private static string[] GetDisksNames()
         {
             return new PerformanceCounterCategory("PhysicalDisk").GetInstanceNames();
         }
 
+        /*
+         * Part of interface, where user decide, which disk will observe
+         */
         public static string SelectDisk()
         {
             int i = 0;
@@ -85,30 +102,37 @@ namespace Kalikowo
                 Console.WriteLine(result);
             }
 
-            if (i == 0)
+            if (i == 0 || i == 1)   //If somethins wrong (in case 1 disk [ex. C:], diskNames contains "C:" and "_Total" 
                 return GetAllDisksString();
 
             Console.Write("\nYour choice: ");
-            string chooseFromCw = Console.ReadKey().KeyChar.ToString();
+            string chooseFromCw = Console.ReadKey().KeyChar.ToString(); //User inputs which disk will observe
+
             int choose;
-            if (!Int32.TryParse(chooseFromCw, out choose))
+            if (!Int32.TryParse(chooseFromCw, out choose))  //If wrong input (int conversion faild)
                 return GetAllDisksString();
 
-            if (choose < 0 && choose > i)
+            if (choose < 0 && choose > i)   //If too small or too big value is given
                 return GetAllDisksString();
 
 
             string selectedDisk = disksNames[choose];
-            Console.WriteLine("\n\nYour choice: " + selectedDisk + " disk \n");
+            if (selectedDisk == ALL_DISKS_DIRECTIVE)    //If all disks selected by user, show other info
+                Console.WriteLine("\n\nYour choice: all disks monitor \n");
+            else
+                Console.WriteLine("\n\nYour choice: " + selectedDisk + " disk \n");
 
 
             return selectedDisk;
         }
 
+        /*
+         * Method to apply all disk monitoring
+         */
         private static string GetAllDisksString()
         {
             Console.WriteLine("\n\nYou choosed all disks monitoring\n");
-            return "_Total";
+            return ALL_DISKS_DIRECTIVE;
         }
     }
 }
